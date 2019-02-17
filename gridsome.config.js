f83186390a09951951d2927ae4e70c9988347cc5
Jeremy H <jhadfield129@gmail.com>
2019-02-17 14:00:24 -0700

    Changed blog directory

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       use: "@gridsome/source-filesystem",
       options: {
-        path: "blog/**/*.md",
+        path: "blog/*.md",
         typeName: "Post"
       }
     }
